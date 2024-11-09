21a421caf7d08377a82bea6a93be05278e5130d6
Olmo Gutiérrez <71456058+Olmo-Gutierrez@users.noreply.github.com>
2024-11-10 00:06:47 +0100

    Update no-classes.js

--- widgets/no-classes.js
+++ widgets/no-classes.js
@@ -3,28 +3,19 @@ async function createCustomWidget() {
   widget.backgroundColor = new Color("#1C1C1E");
   widget.setPadding(16, 16, 16, 16);
   
-  // You can make API calls, fetch data, or create any widget layout you want here
   const stack = widget.addStack();
   stack.layoutVertically();
   
-  const emoji = stack.addText("🌟");
+  const emoji = stack.addText("😴");
   emoji.font = Font.systemFont(32);
   emoji.centerAlignText();
   
   stack.addSpacer(8);
   
-  const title = stack.addText("Free Time!");
+  const title = stack.addText("Time to Rest!");
   title.textColor = Color.white();
   title.font = Font.boldSystemFont(16);
   title.centerAlignText();
-  
-  stack.addSpacer(4);
-  
-  // You could fetch a random quote or message
-  const message = stack.addText("Time to relax or study");
-  message.textColor = new Color("#999999");
-  message.font = Font.systemFont(14);
-  message.centerAlignText();
   
   return widget;
 }
